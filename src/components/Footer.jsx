268a9e4e797a2ef4c80e0b2a1d9b51ddd64d84e2
Raja Taha <rajataha062@gmail.com>
2023-07-05 13:40:12 +0500

    1:40

---
+++ src/components/Footer.jsx
@@ -0,0 +1,61 @@
+import { Box, Button, HStack, Heading, Input, Stack, Text, VStack } from '@chakra-ui/react'
+import { AiOutlineSend, AiFillLinkedin, AiFillGithub, AiFillInstagram} from 'react-icons/ai'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <Box bgColor={'blackAlpha.900'} minH={'40'} p={'16'} color={'white'}>
+        <Stack direction={['column', 'row']}>
+            <VStack alignItems={'stretch'} w={'full'} px={'4'}>
+                <Heading size={'md'} textTransform={'uppercase'} textAlign={['center', 'left']}>
+                    Subscribe to get updates
+                </Heading>
+                <HStack borderBottom={'2px solid white'} py={'2'}>
+                    <Input 
+                        placeholder='Enter Email Here...' 
+                        border={'none'} 
+                        borderRadius={'none'} 
+                        outline={'none'} 
+                        focusBorderColor='none'
+                    />
+                    <Button
+                        p={'0'}
+                        colorScheme='purple'
+                        variant={'ghost'}
+                        borderRadius={'0 20px 20px 0'}
+                    >
+                        <AiOutlineSend size={'20'}/>
+                    </Button>
+                </HStack>
+            </VStack>
+            <VStack 
+                w={'full'} 
+                borderLeft={['none', '2px solid white']}
+                borderRight={['none', '2px solid white']}
+            >
+                <Heading textTransform={'uppercase'} textAlign={'center'}>VIDEO HUB</Heading>
+                <Text>All rights reserved</Text>
+                <Text>{'</>'} with ❤ by Muhammad Taha</Text>
+            </VStack>
+            <VStack
+                w={'full'} 
+            >
+                <Heading size={'md'} textTransform={'uppercase'}>Social Media</Heading>
+                <HStack width={'full'} justifyContent={'space-evenly'}>
+                    <Button variant={'link'} colorScheme='whiteAlpha'>
+                        <a href='https://www.linkedin.com/in/muhammad-taha-b80a56243/' target='blank'><AiFillLinkedin  size={'40'}/></a>
+                    </Button>
+                    <Button variant={'link'} colorScheme='whiteAlpha'>
+                        <a href='https://github.com/MrRajaG' target='blank'><AiFillGithub size={'40'}/></a>
+                    </Button>
+                    <Button variant={'link'} colorScheme='whiteAlpha'>
+                        <a href='https://www.instagram.com/rajataha062/' target='blank'><AiFillInstagram size={'40'}/></a>
+                    </Button>
+                </HStack>
+            </VStack>
+        </Stack>
+    </Box>
+  )
+}
+
+export default Footer
---
+++ src/components/Home.jsx
@@ -0,0 +1,82 @@
+import { Box, Container, Heading, Image, Stack, Text} from '@chakra-ui/react'
+import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import img1 from '../assets/1.jpg'
+import img2 from '../assets/2.jpg'
+import img3 from '../assets/3.jpg'
+import img4 from '../assets/4.jpg'
+import img5 from '../assets/5.png'
+
+const headingOptions = {
+    pos: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+    textTransform: 'uppercase',
+    p: '4',
+    size: '4xl'
+}
+
+const Home = () => {
+  return (
+    <Box>
+        <MyCarousel/>
+        <Container maxW={'container.xl'} minH={'100vh'} p={'16'}>
+            <Heading 
+                textTransform={'uppercase'} 
+                py={'2'} 
+                w={'fit-content'} 
+                borderBottom={'2px solid'} 
+                m={'auto'}
+            >Services</Heading>
+            <Stack 
+                h={'full'}
+                p={'4'}
+                alignItems={'center'}
+                direction={['column', 'row']}
+            >
+                <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'}></Image>
+                <Text 
+                    letterSpacing={'widest'}
+                    lineHeight={'190%'}
+                    p={['4', '16']}
+                    textAlign={'center'}
+                >
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam culpa iste voluptatem voluptas numquam illum minima eius fugit, perferendis, asperiores tempore nihil, suscipit velit sequi possimus natus a similique porro voluptatibus voluptates deleniti molestias. Necessitatibus iure atque dignissimos molestiae saepe velit, sunt dolor adipisci perspiciatis, quam sequi rerum magni?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto rerum quam repudiandae laboriosam aut fugit nulla nihil. Voluptatem consequatur, id numquam excepturi laudantium dolores eveniet, amet veniam ab dicta animi explicabo harum illo nulla totam perspiciatis, corporis autem tempore et itaque recusandae obcaecati? Omnis voluptate repudiandae possimus consectetur molestias impedit.</Text>
+            </Stack>
+        </Container>
+    </Box>
+  )
+}
+
+const MyCarousel = () => (
+    <Carousel autoPlay infiniteLoop interval={2000} showStatus={false} showThumbs={false} showArrows={false}>
+        <Box w={'full'} h={'100vh'}>
+            <Image src={img1}/>
+            <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+                Watch The Future
+            </Heading>
+        </Box>
+        <Box w={'full'} h={'100vh'}>
+            <Image src={img2}/>
+            <Heading bgColor={'whiteAlpha.900'} color={'balck'} {...headingOptions}>
+                Future is Gaming
+            </Heading>
+        </Box>
+        <Box w={'full'} h={'100vh'}>
+            <Image src={img3}/>
+            <Heading bgColor={'whiteAlpha.600'} color={'balck'} {...headingOptions}>
+                Gaming on Console
+            </Heading>
+        </Box>
+        <Box w={'full'} h={'100vh'}>
+            <Image src={img4}/>
+            <Heading bgColor={'whiteAlpha.600'} color={'balck'} {...headingOptions}>
+                Night Life is Cool
+            </Heading>
+        </Box>
+    </Carousel>
+);
+
+export default Home
